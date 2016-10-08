60daba8de3353c91391973e32073d2ac512ecc37
zwlovejava <zwlovejava@163.com>
2016-10-08 15:23:39 +0800

    测试所有模块一起提交

---
+++ stu-core/src/main/java/com/zw/stu/stu_core/App.java
@@ -0,0 +1,13 @@
+package com.zw.stu.stu_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
